554705f19ad2a74616732fe5fb389a6abb236207
Brandon Mowat <me@brandonmowat.com>
2020-08-21 22:36:29 -0400

    clean up package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,22 +35,22 @@ module.exports = {
         name: "static",
       },
     },
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          {
-            resolve: "gatsby-remark-responsive-iframe",
-            options: {
-              wrapperStyle: "margin-bottom: 1.0725rem",
-            },
-          },
-          "gatsby-remark-prismjs",
-          "gatsby-remark-copy-linked-files",
-          "gatsby-remark-smartypants",
-        ],
-      },
-    },
+    // {
+    //   resolve: "gatsby-transformer-remark",
+    //   options: {
+    //     plugins: [
+    //       {
+    //         resolve: "gatsby-remark-responsive-iframe",
+    //         options: {
+    //           wrapperStyle: "margin-bottom: 1.0725rem",
+    //         },
+    //       },
+    //       "gatsby-remark-prismjs",
+    //       "gatsby-remark-copy-linked-files",
+    //       "gatsby-remark-smartypants",
+    //     ],
+    //   },
+    // },
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     {
@@ -71,8 +71,8 @@ module.exports = {
         icon: "content/assets/gatsby-icon.png",
       },
     },
-    "gatsby-plugin-offline",
-    "gatsby-plugin-react-helmet",
+    // "gatsby-plugin-offline",
+    // "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-typography",
       options: {
