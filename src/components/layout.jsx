38b772ac726b590d12a44572e4480a7931177f95
Brandon Mowat <me@brandonmowat.com>
2021-01-02 16:43:09 -0500

    Add new search!

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,32 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import "./layout.css"
 
 const Layout = props => {
-  const { location, title, children } = props
+  const { location, title, children, topicQuery, setTopicQuery } = props
+
+
   const rootPath = `${__PATH_PREFIX__}/`
+
+  console.log(rootPath, window.location.pathname)
   let header
 
-  if (location.pathname === rootPath) {
+  if (window.location.pathname !== rootPath) {
     header = (
-      <div className="header-content">
-        <h1>
-          <Link
-            style={{
-              boxShadow: "none",
-              textDecoration: "none",
-              color: "inherit",
-            }}
-            to="/"
-          >
-            <img src="../../MM.png" />
-          </Link>
-        </h1>
-      </div>
-    )
-  } else {
-    header = (
-      <div className="container header-content">
+      <div className="container header">
         <div className="blog-title">
           <Link
             style={{
@@ -39,7 +26,35 @@ const Layout = props => {
             <img src="../../MM.png" />
           </Link>
         </div>
-        <h3 className="description">a blog written by Brandon Mowat</h3>
+      </div>
+    )
+  } else {
+    header = (
+      <div className="container header">
+        <div className="blog-title">
+          <Link
+            style={{
+              boxShadow: "none",
+              textDecoration: "none",
+              color: "inherit",
+            }}
+            to="/"
+          >
+            <img src="../../MM.png" />
+          </Link>
+          <h3 className="description">a blog written by Brandon Mowat</h3>
+        </div>
+        <div className="header-content">
+          <input
+            className="SearchBar"
+            placeholder="Search for something..."
+            value={topicQuery} onChange={e => setTopicQuery(e.target.value)}/>
+          <div className="header-content-topics">
+            <button onClick={() => setTopicQuery("running")}>Running</button>
+            <button onClick={() => setTopicQuery("cooking")}>Cooking</button>
+            <button onClick={() => setTopicQuery("ada")}>Ada</button>
+          </div>
+        </div>
       </div>
     )
   }
--- src/templates/home.jsx
+++ src/templates/home.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
@@ -12,20 +12,20 @@ import '../components/layout.css';
 import './pages.css';
 
 const BlogIndex = props => {
+  const [topicQuery, setTopicQuery] = useState("");
   const { pageContext } = props;
   const { articles } = pageContext;
+
   articles.sort((a, b) => (new Date(a.publishedDate || a.created) < new Date(b.publishedDate || b.created) ? 1 : -1));
+
+  const articlesOnTopic = articles.filter(article => article.tags.includes(topicQuery))
 
   if (!articles) {
     return <div>no posts</div>;
   }
 
-  const firstArticle = articles[0];
-
-  const restArticles = articles.slice(1);
-
   return (
-    <Layout location="/" title="Macha & Mochi">
+    <Layout location="/" title="Macha & Mochi" topicQuery={topicQuery} setTopicQuery={setTopicQuery}>
       <SEO
         title="All posts"
         keywords={[
@@ -38,50 +38,90 @@ const BlogIndex = props => {
           'mochi',
         ]}
       />
-      {/* <Bio /> */}
-      <div className="row">
-        <span className="LatestPostLabel">Latest Post</span>
-      </div>
 
-      <Link className="Post FeaturedArticle" to={`article/${firstArticle._id}`}>
-        <section>
-          <div className="row">
-            <div className="col-2">
-              <h5 className="FeaturedArticle__date">
-                {new Date(firstArticle.publishedDate || firstArticle.created).toDateString()}
-              </h5>
-            </div>
-            <div className="col-4">
-              <h3>{firstArticle.title}</h3>
-              <h5>{firstArticle.description}</h5>
-            </div>
-          </div>
-        </section>
-      </Link>
+      {
+        topicQuery ?
+        <SearchResults articles={articles} topicQuery={topicQuery} /> :
+        <MainArticleView articles={articles} />
+      }
 
-      <div className="row">
-        <span>Older Posts</span>
-      </div>
-
-      {restArticles.map(article => (
-        <Link className="Post" to={`article/${article._id}`}>
-          <div className="row">
-            <div className="col-2">
-              <h5 className="Post__date">
-                {new Date(article.publishedDate || article.created).toDateString()}
-              </h5>
-            </div>
-            <div className="col-4">
-              <h3>{article.title}</h3>
-              <h5>{article.description}</h5>
-            </div>
-          </div>
-        </Link>
-      ))}
       <Bio />
     </Layout>
   );
 };
+
+const MainArticleView = props => {
+
+  const { articles } = props;
+
+  const firstArticle = articles[0];
+  const restArticles = articles.slice(1);
+
+  return (<>
+    <div className="row">
+      <span className="LatestPostLabel">Latest Post</span>
+    </div>
+
+    <Link className="Post FeaturedArticle" to={`article/${firstArticle._id}`}>
+      <section>
+        <div className="row">
+          <div className="col-2">
+            <h5 className="FeaturedArticle__date">
+              {new Date(firstArticle.publishedDate || firstArticle.created).toDateString()}
+            </h5>
+          </div>
+          <div className="col-4">
+            <h3>{firstArticle.title}</h3>
+            <h5>{firstArticle.description}</h5>
+          </div>
+        </div>
+      </section>
+    </Link>
+
+    <div className="row">
+      <span>Older Posts</span>
+    </div>
+
+    {restArticles.map(article => (
+      <Link className="Post" to={`article/${article._id}`}>
+        <div className="row">
+          <div className="col-2">
+            <h5 className="Post__date">
+              {new Date(article.publishedDate || article.created).toDateString()}
+            </h5>
+          </div>
+          <div className="col-4">
+            <h3>{article.title}</h3>
+            <h5>{article.description}</h5>
+          </div>
+        </div>
+      </Link>
+    ))}
+  </>);
+};
+
+const SearchResults = props => {
+  const { articles, topicQuery } = props;
+  const articlesOnTopic = articles.filter(article => article.tags.includes(topicQuery))
+
+  return (
+    articlesOnTopic.map(article => (
+      <Link className="Post" to={`article/${article._id}`}>
+        <div className="row">
+          <div className="col-2">
+            <h5 className="Post__date">
+              {new Date(article.publishedDate || article.created).toDateString()}
+            </h5>
+          </div>
+          <div className="col-4">
+            <h3>{article.title}</h3>
+            <h5>{article.description}</h5>
+          </div>
+        </div>
+      </Link>
+    ))
+  );
+}
 
 export default BlogIndex;
 
