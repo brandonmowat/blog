711328349d36e81a3c55c2a1ee3b04ab18f25f0c
Brandon Mowat <me@brandonmowat.com>
2022-01-29 19:32:46 -0500

    remove broken import

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -9,7 +9,6 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
-import mc from "../utils/mailchimp-script";
 
 function SEO({
   description, lang, meta, title, tags
